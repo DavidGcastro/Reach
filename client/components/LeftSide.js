6a78b3fce5e7bba38e35dec41f4cd48d86514db5
David <davidgcastro93@gmail.com>
2018-08-27 21:55:25 -0400

    transitions work to setting form

--- client/components/LeftSide.js
+++ client/components/LeftSide.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import Welcome from './Welcome';
+import Settings from './Settings';
 import { CSSTransition } from 'react-transition-group';
 
 class LeftSide extends React.Component {
   constructor() {
     super();
     this.state = {
-      visible: true
+      visible: true,
+      initialView: true
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this.state);
+    this.setState({ initialView: false, visible: true });
   }
   render() {
+    let initialView = this.state.initialView ? (
+      <div className="flexer">
+        <h1>The</h1>
+        <h1>Great</h1>
+        <h1>Guessing Game</h1>
+        <button
+          onClick={() => this.handleClick()}
+          type="button"
+          className="button">
+          Begin
+        </button>
+      </div>
+    ) : (
+      <CSSTransition
+        in={this.state.visible}
+        timeout={8000}
+        classNames="fade"
+        appear={true}>
+        <Settings />
+      </CSSTransition>
+    );
     return (
       <div className="left--parent">
-        <div className="left--currentContent">
-          <CSSTransition
-            in={this.state.visible}
-            appear={true}
-            
-            classNames="fade">
-            <Welcome />
-          </CSSTransition>
-        </div>
+        <div className="left--currentContent">{initialView}</div>
       </div>
     );
   }
