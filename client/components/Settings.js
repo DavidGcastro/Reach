18cf465e2ce3926a00e5e2b20815b1fdec2a735a
David <davidgcastro93@gmail.com>
2018-08-28 01:18:41 -0400

    transition work

--- client/components/Settings.js
+++ client/components/Settings.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
+import { CSSTransition } from 'react-transition-group';
 export default class Settings extends React.Component {
   constructor() {
     super();
@@ -28,42 +28,49 @@ export default class Settings extends React.Component {
 
   render() {
     return (
-      <form>
-        <div style={{ marginBottom: '20px' }}>
-          <label name="name" style={{ display: 'block', fontSize: '4vh' }}>
-            Name
-          </label>
-          <input
-            onChange={event => this.handleName(event)}
-            style={{ padding: 10 }}
-            placeholder="Enter your name."
-          />
-        </div>
-        <div style={{ marginBottom: '20px' }}>
-          <label
-            name="difficulty"
-            style={{ display: 'block', fontSize: '4vh' }}>
-            Enter Difficulty
-          </label>
-          <input
-            onChange={event => this.handleDifficulty(event)}
-            min="1"
-            max="10"
-            type="number"
-            style={{ width: '100%', padding: 10 }}
-            placeholder="Enter your Difficulty."
-          />
-        </div>
-        <Link
-          to={{
-            pathname: '/start',
-            state: this.state
-          }}>
-          <button type="button" className="button" style={{ width: '100%' }}>
-            Enter
-          </button>
-        </Link>
-      </form>
+      <CSSTransition
+        in={true}
+        appear
+        timeout={800}
+        classNames="fade"
+        unmountOnExit>
+        <form>
+          <div style={{ marginBottom: '20px' }}>
+            <label name="name" style={{ display: 'block', fontSize: '4vh' }}>
+              Name
+            </label>
+            <input
+              onChange={event => this.handleName(event)}
+              style={{ padding: 10, borderRadius: 5 }}
+              placeholder="Enter your name."
+            />
+          </div>
+          <div style={{ marginBottom: '20px' }}>
+            <label
+              name="difficulty"
+              style={{ display: 'block', fontSize: '4vh' }}>
+              Enter Difficulty
+            </label>
+            <input
+              onChange={event => this.handleDifficulty(event)}
+              min="1"
+              max="10"
+              type="number"
+              style={{ width: '100%', padding: 10, borderRadius: 5 }}
+              placeholder="Enter your Difficulty."
+            />
+          </div>
+          <Link
+            to={{
+              pathname: '/start',
+              state: this.state
+            }}>
+            <button type="button" className="button" style={{ width: '100%' }}>
+              Enter
+            </button>
+          </Link>
+        </form>
+      </CSSTransition>
     );
   }
 }
--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 
 const Welcome = () => {
   return (
-    <div className="flexer">
-      <h1>The</h1>
-      <h1>Great</h1>
-      <h1>Guessing Game</h1>
-      <Link to="/settings">
-        <button style={{ width: '100%' }} type="button" className="button">
-          Begin
-        </button>
-      </Link>
-    </div>
+    <CSSTransition
+      in={true}
+      appear
+      timeout={800}
+      classNames="fade"
+      unmountOnExit>
+      <div className="flexer">
+        <h1>The</h1>
+        <h1>Great</h1>
+        <h1>Guessing Game</h1>
+        <Link to="/settings">
+          <button style={{ width: '100%' }} type="button" className="button">
+            Begin
+          </button>
+        </Link>
+      </div>
+    </CSSTransition>
   );
 };
 
