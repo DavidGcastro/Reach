7587e92e276a6dce5813aaa77ddf86fc3af1ad80
David <davidgcastro93@gmail.com>
2018-08-28 02:08:04 -0400

    Finished Left side

--- client/components/GameStart.js
+++ client/components/GameStart.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import axios from 'axios';
+import { CSSTransition } from 'react-transition-group';
 
 export default class GameStart extends React.Component {
   constructor() {
     super();
     this.state = {
       words: '',
-      chosenWord: ''
+      chosenWord: '',
+      guesses: 0,
+      currentGuess: ''
     };
     this.getRandomWord = this.getRandomWord.bind(this);
   }
@@ -22,7 +25,7 @@ export default class GameStart extends React.Component {
     return axios
       .get('http://app.linkedin-reach.io/words', {
         params: {
-          difficulty: Number(difficulty),
+          difficulty: Number(difficulty) || 5,
           minLength,
           maxLength,
           start,
@@ -44,6 +47,44 @@ export default class GameStart extends React.Component {
   }
 
   render() {
-    return <h1>{this.state.chosenWord}</h1>;
+    console.log(this.state.chosenWord);
+    let splitWord = this.state.chosenWord.split('');
+    return (
+      <CSSTransition
+        in={true}
+        appear
+        timeout={500}
+        classNames="fade"
+        unmountOnExit>
+        <div
+          style={{
+            justifyContent: 'space-evenly',
+            display: 'flex',
+            flex: 1,
+            flexDirection: 'column'
+          }}>
+          <h1>Guess Wisely. </h1>
+          <input
+            type="text"
+            placeHolder="Type Letter or Phrase"
+            style={{ padding: 10 }}
+          />
+          <div style={{ flexDirection: 'row', display: 'flex' }}>
+            {splitWord.map((letter, index) => {
+              return (
+                <div
+                  key={letter + index}
+                  style={{
+                    borderBottom: '3px solid',
+                    margin: 5,
+                    width: '50px'
+                  }}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </CSSTransition>
+    );
   }
 }
--- client/components/RightSide.js
+++ client/components/RightSide.js
@@ -2,8 +2,70 @@ import React from 'react';
 
 const RightSide = props => {
   return (
-    <div className="right--parent">
-      <h1 />
+    <div className="right--parent flexer">
+      <div
+        id="hangman-figure"
+        className="d-flex justify-content-center pt-5 pb-3">
+        <div id="gallow">
+          <div id="pole">
+            <div id="noose" />
+            <div id="diagonal" />
+            <div id="base" />
+          </div>
+        </div>
+
+        <div id="stick-figure" className="swinging">
+          <svg height="150" width="150">
+            <circle
+              id="hangman-head"
+              className="show-hangman"
+              cx="25"
+              cy="25"
+              r="10"
+            />
+            <line
+              id="hangman-body"
+              className="show-hangman"
+              x1="25"
+              y1="34"
+              x2="25"
+              y2="85"
+            />
+            <line
+              id="hangman-leftArm"
+              className="show-hangman"
+              x1="0"
+              y1="70"
+              x2="25"
+              y2="40"
+            />
+            <line
+              id="hangman-rightArm"
+              className="show-hangman"
+              x1="25"
+              y1="40"
+              x2="55"
+              y2="70"
+            />
+            <line
+              id="hangman-leftLeg"
+              className="show-hangman"
+              x1="25"
+              y1="85"
+              x2="0"
+              y2="120"
+            />
+            <line
+              id="hangman-rightLeg"
+              className="show-hangman"
+              x1="25"
+              y1="85"
+              x2="55"
+              y2="120"
+            />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 };
