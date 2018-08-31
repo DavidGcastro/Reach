46f52e7c5a4014b3786f4053e4b35e09d4983560
David <davidgcastro93@gmail.com>
2018-08-30 23:09:18 -0400

    adjusted server

--- server/api/words.js
+++ server/api/words.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const CircularJSON = require('circular-json');
 
 router.get('/:difficulty', (req, res, next) => {
-  let difficulty = req.params.difficulty ? req.params.difficulty : 5;
+  let difficulty = req.params.difficulty;
   axios
     .get(
       `http://app.linkedin-reach.io/words?difficulty=${difficulty}&minLength=3&maxLength=10&start=0&count=500`
@@ -12,7 +12,6 @@ router.get('/:difficulty', (req, res, next) => {
       let json = CircularJSON.stringify(response.data);
       return json;
     })
-    .then(x => x)
     .then(words => res.send(words))
     .catch(error => {
       console.log(error);
