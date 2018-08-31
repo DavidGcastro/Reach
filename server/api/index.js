bf761e3ba95fa8471d21888417e5dbefc8d825af
David <davidgcastro93@gmail.com>
2018-08-30 23:13:19 -0400

    deleted files

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 //Map to api files
-// starting from /api
+
 router.use('/highScores', require('./highscores'));
 router.use('/words', require('./words'));
 
