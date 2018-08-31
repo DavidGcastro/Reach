dbe1bac7bd8f12898ef61949f74b151b6f609d58
David <davidgcastro93@gmail.com>
2018-08-30 22:41:00 -0400

    api is working

--- server/api/words.js
+++ server/api/words.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const axios = require('axios');
 const CircularJSON = require('circular-json');
 
-router.get('/', (req, res, next) => {
+router.get('/:difficulty', (req, res, next) => {
+  let difficulty = req.params.difficulty ? req.params.difficulty : 5;
   axios
-    .get('http://app.linkedin-reach.io/words')
+    .get(
+      `http://app.linkedin-reach.io/words?difficulty=${difficulty}&minLength=3&maxLength=10&start=0&count=500`
+    )
     .then(response => {
       let json = CircularJSON.stringify(response.data);
       return json;
